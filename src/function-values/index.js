f8291c515a1b2656b410118b6cef65db40d0b870
Vitaly Smirnov <vitaly.smirnov@nexign.com>
2023-01-25 19:37:14 +0000

    Added solution for algorithm function-values

---
+++ src/function-values/index.js
@@ -0,0 +1,13 @@
+// const a = -8;
+// const x = -5;
+// const b = -2;
+// const c = 7;
+
+const arrNumberInput = [-8, -5, -2, 7];
+
+const [a, x, b, c] = arrNumberInput;
+
+const answer = a * (x * x) + b * x + c;
+
+console.log(typeof `${a * (x * x) + b * x + c}`);
+console.log(`${a * (x * x) + b * x + c}`);
